bbd043c39b452ea1c920e463c5e3465c79902451
catLee <leemiyinghao@gmail.com>
2014-12-11 04:08:52 +0900

    remove testing OAuth Key.

--- config.php
+++ config.php
@@ -5,6 +5,6 @@
  * A single location to store configuration.
  */
 
-define('CONSUMER_KEY', '8QvzdGGyBWQs2uEFed3X1puUH');
-define('CONSUMER_SECRET', '9iNY9bV4z0ssnvPpyi61b77nJ8RiTvgPt9ofqofXihGX0R379l');
-define('OAUTH_CALLBACK', 'http://longcat.tw/catLee/i-vote/callback.php');
+define('CONSUMER_KEY', 'CONSUMER_KEY_HERE');
+define('CONSUMER_SECRET', 'CONSUMER_SECRET_HERE');
+define('OAUTH_CALLBACK', 'OAUTH_CALLBACK_HERE');
